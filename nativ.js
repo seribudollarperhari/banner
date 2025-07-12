11b6b52849dc94a03d2b87cab90810c70ff2c1d4
seribudollarperhari <ardiansyah.erlando@gmail.com>
2025-07-13 06:08:31 +0700

    Create nativ.js

---
+++ nativ.js
@@ -0,0 +1,4 @@
+document.write(`
+  <script async="async" data-cfasync="false" src="//roastwearwritten.com/6683f4839a05cff47fc63acd1bd5b87e/invoke.js"><\/script>
+  <div id="container-6683f4839a05cff47fc63acd1bd5b87e"></div>
+`);
