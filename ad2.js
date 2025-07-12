50825fc01885cc36af329cd26d955f25377a8177
seribudollarperhari <ardiansyah.erlando@gmail.com>
2025-07-13 05:51:34 +0700

    Create ad2.js

---
+++ ad2.js
@@ -0,0 +1,12 @@
+document.write(`
+<script type="text/javascript">
+    atOptions = {
+        'key' : '392fbd37fc20834e2ea77f031022ab0b',
+        'format' : 'iframe',
+        'height' : 250,
+        'width' : 300,
+        'params' : {}
+    };
+<\/script>
+<script type="text/javascript" src="//roastwearwritten.com/392fbd37fc20834e2ea77f031022ab0b/invoke.js"><\/script>
+`);
