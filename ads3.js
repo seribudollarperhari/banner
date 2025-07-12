68f34b40284db7256db425b8a4f6b0f624a746f5
seribudollarperhari <ardiansyah.erlando@gmail.com>
2025-07-13 06:13:05 +0700

    Create ads3.js

---
+++ ads3.js
@@ -0,0 +1,12 @@
+document.write(`
+<script type="text/javascript">
+    atOptions = {
+        'key' : 'a40e357bbbf7bc1f640327874bfcdfa4',
+        'format' : 'iframe',
+        'height' : 250,
+        'width' : 300,
+        'params' : {}
+    };
+<\/script>
+<script type="text/javascript" src="//www.highperformanceformat.com/a40e357bbbf7bc1f640327874bfcdfa4/invoke.js"><\/script>
+`);
