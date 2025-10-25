21418330794f837fb841d0694a58e72eaa1e7564
seribudollarperhari <ardiansyah.erlando@gmail.com>
2025-10-26 05:05:34 +0700

    Update socialbar.js

--- socialbar.js
+++ socialbar.js
@@ -1,1 +1,1 @@
-document.write("<script src='//roastwearwritten.com/04/32/b3/0432b39f07b824e25daa06eba65aa973.js' type='text/javascript'><\/script>");
+document.write("<script src='//faciliatemate.com/04/32/b3/0432b39f07b824e25daa06eba65aa973.js' type='text/javascript'><\/script>");
