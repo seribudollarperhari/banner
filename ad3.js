209600a4dc348307a1a1c7cf8dfa45ca83501b96
seribudollarperhari <ardiansyah.erlando@gmail.com>
2026-01-29 18:32:22 +0700

    Update ad3.js

--- ad3.js
+++ ad3.js
@@ -1,12 +1,12 @@
 document.write(`
 <script>
   atOptions = {
-    'key' : '392fbd37fc20834e2ea77f031022ab0b',
+    'key' : 'a317173734fbcd8901b885b17a227f72',
     'format' : 'iframe',
     'height' : 250,
     'width' : 300,
     'params' : {}
   };
 <\/script>
-<script src="//percussivetakelaunch.com/392fbd37fc20834e2ea77f031022ab0b/invoke.js"><\/script>
+<script src="//exasperatebubblyorthodox.com/a317173734fbcd8901b885b17a227f72/invoke.js"><\/script>
 `);
