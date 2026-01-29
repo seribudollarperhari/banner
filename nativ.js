09022df8c8cff3fa0d4a32b500f72eaa5ba852f5
seribudollarperhari <ardiansyah.erlando@gmail.com>
2026-01-29 18:33:22 +0700

    Update nativ.js

--- nativ.js
+++ nativ.js
@@ -1,4 +1,4 @@
 document.write(`
-<script async="async" data-cfasync="false" src="//percussivetakelaunch.com/6683f4839a05cff47fc63acd1bd5b87e/invoke.js"><\/script>
+<script async="async" data-cfasync="false" src="//exasperatebubblyorthodox.com/6683f4839a05cff47fc63acd1bd5b87e/invoke.js"><\/script>
 <div id="container-6683f4839a05cff47fc63acd1bd5b87e"></div>
 `);
