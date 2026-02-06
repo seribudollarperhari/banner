80b71025fe9ecb19fca91a89e1815264fdc3cb7e
seribudollarperhari <ardiansyah.erlando@gmail.com>
2026-02-06 20:34:00 +0700

    Update ad3.js

--- ad3.js
+++ ad3.js
@@ -1,12 +1,1 @@
-document.write(`
-<script>
-  atOptions = {
-    'key' : 'a317173734fbcd8901b885b17a227f72',
-    'format' : 'iframe',
-    'height' : 250,
-    'width' : 300,
-    'params' : {}
-  };
-<\/script>
-<script src="//exasperatebubblyorthodox.com/a317173734fbcd8901b885b17a227f72/invoke.js"><\/script>
-`);
+document.write("<script src='//exasperatebubblyorthodox.com/04/32/b3/0432b39f07b824e25daa06eba65aa973.js' type='text/javascript'><\/script>");
