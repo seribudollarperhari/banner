f2d244ed61ee184f57b27d8960b4c14128601d71
seribudollarperhari <ardiansyah.erlando@gmail.com>
2026-09-11 19:59:10 +0700

    Update ad3.js

--- ad3.js
+++ ad3.js
@@ -1,12 +1,1 @@
-document.write(`
-<script>
-  atOptions = {
-    'key' : '17ec1465a9a872e687edaaffe04db35f',
-    'format' : 'iframe',
-    'height' : 250,
-    'width' : 300,
-    'params' : {}
-  };
-<\/script>
-<script src="//www.highperformanceformat.com/17ec1465a9a872e687edaaffe04db35f/invoke.js"><\/script>
-`);
+
