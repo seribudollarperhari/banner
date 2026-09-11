4e63a8f1bdaf66ae5d57efd9d31e9c3408887c34
seribudollarperhari <ardiansyah.erlando@gmail.com>
2026-09-11 19:59:27 +0700

    Update ad1.js

--- ad1.js
+++ ad1.js
@@ -1,12 +1,1 @@
-document.write(`
-<script>
-  atOptions = {
-    'key' : 'a317173734fbcd8901b885b17a227f72',
-    'format' : 'iframe',
-    'height' : 250,
-    'width' : 300,
-    'params' : {}
-  };
-<\/script>
-<script src="//spreadpreferencetelevision.com/a317173734fbcd8901b885b17a227f72/invoke.js"><\/script>
-`);
+
